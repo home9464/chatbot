445b3951c49c2b62cb7c5737df58ae97e50c0e8d
home9464@gmail.com <home9464@gmail.com>
2018-11-09 00:47:52 -0800

    up

--- train.py
+++ train.py
@@ -12,7 +12,12 @@ from search import GreedySearchDecoder
 
 import params
 
+
 def maskNLLLoss(inp, target, mask):
+    """
+    Args:
+    
+    """
     nTotal = mask.sum()
     crossEntropy = -torch.log(torch.gather(inp, 1, target.view(-1, 1)))
     loss = crossEntropy.masked_select(mask).mean()
